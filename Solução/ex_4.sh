f62077498da049136cc77fa0a54923379b620a81
Luís Felipe Marciano <lfamarciano@gmail.com>
2023-04-01 17:10:47 -0300

    fiz os ex 5 e 6

--- Solução/ex_4.sh
+++ Solução/ex_4.sh
@@ -1,1 +1,1 @@
-sed 's/ /\n/g' ../dados/machado/contos/macn001.txt | sed '/^$/d' | tr -d '[:punct:]' | LC_ALL='C' sort -bdf  | uniq -i -c | LC_ALL='C' sort -rn -k 1 > resposta_4.txt
+sed 's/ /\n/g' ../dados/machado/contos/macn001.txt | sed '/^$/d' | tr -d '[!"#$%&()*+,\./:;<=>?@[\]^_`{|}~]' | LC_ALL='C' sort -bdf  | uniq -i -c | LC_ALL='C' sort -rn -k 1 > resposta_4.txt
